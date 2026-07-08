1fe007d9d7aa8d5d47dda603f6242a9f97097bce
Tsegaab Alemu <tsegaabalemu147@gmail.com>
2026-07-08 16:08:59 +0300

    updated

---
+++ src/types/react-router-dom.d.ts
@@ -0,0 +1,12 @@
+declare module 'react-router-dom' {
+  export const Routes: any;
+  export const Route: any;
+  export const Link: any;
+  export const NavLink: any;
+  export const BrowserRouter: any;
+  export const useNavigate: any;
+  export const useParams: any;
+  export const Outlet: any;
+  const _default: any;
+  export default _default;
+}
